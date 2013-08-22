3a772a31615c9724c878bfc525a21c72fba2742f
Cody Goodman <codygman.consulting@gmail.com>
2013-08-22 11:55:44 -0500

    Allow '+' in verify links

--- hunger/urls.py
+++ hunger/urls.py
@@ -3,7 +3,7 @@ from .views import InviteView, VerifiedView, InvalidView, NotBetaView, InviteSen
 
 
 urlpatterns = patterns('',
-    url(r'^verify/(\w+)/$', 'hunger.views.verify_invite', name='hunger-verify'),
+    url(r'^verify/([\w\+]+)/$', 'hunger.views.verify_invite', name='hunger-verify'),
     url(r'^invite/$', InviteView.as_view(), name='hunger-invite'),
     url(r'^sent/$', InviteSentView, name='hunger-invite-sent'),
     url(r'^not-in-beta/$', NotBetaView.as_view(), name='hunger-not-in-beta'),
